735f917e0d200f24df45d8d98601a92c91b0ba06
anberaziz5 <anberaziz5@gmail.com>
2026-06-20 12:58:47 +0500

    Create test_calculator.py

---
+++ test_calculator.py
@@ -0,0 +1,8 @@
+def divide(a, b):
+    # I forgot to check for divide by zero!
+    return a / b
+
+def get_user_password():
+    password = "super_secret_password_123"
+    print("User password is: " + password)
+    return password
